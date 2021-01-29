a32472cc2f31edd1ea99a82984bf1349031ade2a
GiusepppeFulgione <74957619+fulgionePy@users.noreply.github.com>
2021-01-29 16:59:55 +0100

    Add files via upload

---
+++ Esercizi/Esercizi/Primo esercizio scooter.py
@@ -0,0 +1,13 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("quanti giorni dura il noleggio?")
+Input: A
+Print ("quanti giorni extra?")
+Input: B  
+If: A = 1 print (45 + (B * 40 ))
+If: A = 2 print (80 + (B * 40 ))
+If: A = 3 print (120 + (B * 40 ))
+If: A = 4 Print (160 + (B * 40 ))
+print ("questo è il costo del tuo noleggio in euro")
+
---
+++ Esercizi/Esercizi/Python Fulgione.py
@@ -0,0 +1,8 @@
+Python 3.9.0 (v3.9.0:9cf6752276, Oct  5 2020, 11:29:23) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numero = 5
+>>> reale = 3.14
+>>> print("Moltiplicando il raggio per 2, ottengo il diametro",numero*2,"moltiplicando quest'ultimo per pi-greco",reale,"otteniamo la misura della circonferenza",numero*2*reale)
+Moltiplicando il raggio per 2, ottengo il diametro 10 moltiplicando quest'ultimo per pi-greco 3.14 otteniamo la misura della circonferenza 31.400000000000002
+>>> 
