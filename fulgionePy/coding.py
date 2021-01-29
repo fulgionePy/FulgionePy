17c26215751af7051b9907a3ecf1621ea38b7fe3
GiusepppeFulgione <74957619+fulgionePy@users.noreply.github.com>
2021-01-29 16:59:18 +0100

    Delete fulgionePy directory

--- fulgionePy/coding.py
+++
@@ -1,7 +0,0 @@
-stringa= input("Scrivere una stringa: ")
-lista_lettere = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-print("i numeri di volte in cui i le lettere si ripetono all'interno della stringa è")
-stringa = list(stringa.upper())
-for lettera in lista_lettere:
-    if stringa.count(lettera) > 0:
-        print(lettera, " = ",stringa.count(lettera))
