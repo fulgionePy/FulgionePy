7dff8e423e2900157727a4012108584efdeae95b
fulgionePy <74957619+fulgionePy@users.noreply.github.com>
2021-02-02 15:17:32 +0100

    Spostato

---
+++ Interfacce/interfaccia.py
@@ -0,0 +1,62 @@
+import string
+import numpy as np
+import matplotlib.pyplot as plt
+from guizero import *
+
+def Grafico (x):
+# apriamo il file in lettura
+    f = open(x.value, 'r')
+
+    # usiamo il metodo readlines 
+    # per ottenere una lista di righe del file
+
+    # stampiamo la prima riga
+    # print(f.readline()) 
+
+    # possiamo fare un ciclo e prendere riga per riga 
+
+    coordX = []
+    coordY = []
+
+    # da notare che posso fare un ciclo all'interno di un file
+    # direttamente sulle righe
+
+    for riga in f:
+        valori = str(f.readline())  # converto in stringa la riga
+        Nval = len(valori)          # conto il numero di caratteri
+        valori = valori.strip('\n') # elimino i lterminatore di riga
+        valori = valori.split(',')  # separo la stringa in due numeri
+        valori = list(valori)       # converto in lista
+        print(valori)
+        coordX.append(int(valori[0])) # aggiungo la coordinata X
+        coordY.append(int(valori[1])) # aggiungo la coordinata Y
+
+    f.close()  # chiudiamo il file
+
+    print ("X: ",coordX)
+    print ("Y: ",coordY)
+
+    # ordiniamo le liste
+    coordX.sort()
+    coordY.sort()
+
+    print("liste ordinate:") 
+    print ("X: ",coordX)
+    print ("Y: ",coordY)
+
+    # stampo il tipo di dati delle coordinate
+    print(type(coordX))
+    print(type(coordY))
+
+    # ora sono pronte per essere usate anche nei grafici
+
+    plt.plot(coordX,coordY, "ro")
+    plt.ylabel('some numbers')
+    plt.savefig('plot.png')
+    picture = Picture(app, image="plot.png")
+
+app= App(title="Grafico")
+etichetta=Text(app,text="scegli file dati.txt")
+percorso= TextBox(app)
+pushGrafico= PushButton(app, text="Genera!", command=Grafico, args=[percorso])
+app.display()
